08aee09ac4e93faed3a9cc1d9da8baa962ef21a8
mandunee <mandudohyun@naver.com>
2025-07-07 02:16:02 +0900

    remove hardcoded api keys

---
+++ src/config/env.ts
@@ -0,0 +1,6 @@
+// Central place for runtime constants.
+// If you don't want to set environment variables manually, fill the placeholders below.
+// NOTE: Do NOT commit real secrets to public repos.
+
+export const OSU_CLIENT_ID: string | undefined = process.env.OSU_CLIENT_ID;
+export const OSU_CLIENT_SECRET: string | undefined = process.env.OSU_CLIENT_SECRET;
